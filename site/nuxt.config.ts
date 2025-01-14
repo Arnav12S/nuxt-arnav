b093b06ea89a6efd686936f6af0ade087bd48380
Arnav12S <66680922+Arnav12S@users.noreply.github.com>
2025-01-14 11:22:29 +0100

    tc

--- site/nuxt.config.ts
+++ site/nuxt.config.ts
@@ -1,66 +1,65 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['@nuxt/ui-pro'],
+  devtools: { enabled: true },
 
   modules: [
-    '@nuxt/content',
+    '@nuxt/ui-pro',
     '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/image',
-    '@nuxt/ui',
-    '@nuxthq/studio',
-    '@vueuse/nuxt',
-    'nuxt-og-image'
+    '@nuxt/content',
+    '@nuxthub/core',
+    '@vueuse/nuxt'
   ],
-
-  devtools: {
-    enabled: true
-  },
-
-  colorMode: {
-    disableTransition: true
-  },
-
-  routeRules: {
-    '/api/search.json': { prerender: true },
-    '/docs': { redirect: '/docs/getting-started', prerender: false }
-  },
 
   future: {
     compatibilityVersion: 4
   },
 
-  compatibilityDate: '2024-07-11',
-
-  nitro: {
-    prerender: {
-      routes: [
-        '/',
-        '/docs'
-      ],
-      crawlLinks: true
-    }
+  content: {
+    studio: {
+      enabled: true,
+    },
+    build: {
+      pathMeta: {
+        forceLeadingSlash: true,
+      },
+      markdown: {
+        toc: {
+          depth: 3,
+          searchDepth: 3,
+        },
+        highlight: {
+          theme: {
+            default: 'vitesse-light',
+            dark: 'vitesse-dark',
+          },
+          langs: [
+            'bash',
+            'css',
+            'diff',
+            'html',
+            'javascript',
+            'js',
+            'json',
+            'markdown',
+            'md',
+            'powershell',
+            'scss',
+            'shell',
+            'ts',
+            'tsx',
+            'typescript',
+            'vue',
+            'xml',
+            'yaml',
+            'cs',
+          ],
+        },
+      },
+    },
   },
-
-  typescript: {
-    strict: false
+  hub: {
+    database: true,
+    cache: true
   },
-
-  hooks: {
-    // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
-    'components:extend': (components) => {
-      const globals = components.filter(c => ['UButton'].includes(c.pascalName))
-
-      globals.forEach(c => c.global = true)
-    }
-  },
-
-  eslint: {
-    config: {
-      stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
-  }
+  compatibilityDate: '2025-01-13'
 })
